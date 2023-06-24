6ec94acaf3d4241d81897f47730b87f9dd4eb29e
elias <eliasab@college.harvard.edu>
2023-06-24 12:45:45 +0200

    expose components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import {BrowserModule, HammerModule} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BasePickerComponent } from './components/pages/base-picker/base-picker.component';
-import { TimePickerComponent } from './components/pages/time-picker/time-picker.component';
+import { BasePickerComponent } from './timeless-picker/components/pages/base-picker/base-picker.component';
+import { TimePickerComponent } from './timeless-picker/components/pages/time-picker/time-picker.component';
 import {FormsModule} from "@angular/forms";
-import { TimelessContainerComponent } from './components/templates/timeless-container/timeless-container.component';
+import { TimelessContainerComponent } from './timeless-picker/components/templates/timeless-container/timeless-container.component';
 
 @NgModule({
   declarations: [
